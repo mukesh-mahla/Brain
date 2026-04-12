d797618b1bdbc4c7a5e76ac2456dd411a44e8bed
Mukesh Mahla <mukesh.23it647@rtu.ac.in>
2026-04-12 19:04:59 +0530

    Fix variable name from '_id' to 'id' in dashboard

--- brainly-frontend/src/pages/dashBoard.tsx
+++ brainly-frontend/src/pages/dashBoard.tsx
@@ -137,7 +137,7 @@ useEffect(()=>{
     ({ _id, type, link, title, summary, tags }) => (
       <div key={_id} className="break-inside-avoid">
         <Card
-          id={_id}
+          id={id}
           type={type}
           link={link}
           title={title}
